5592551d89188201f035924164fea197f5dcd34f
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-27 16:18:54 +0430

    refactor

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Main from "pages/main/Main";
 import Comments from "pages/comments/Comments";
 
 function App() {
-  return (
-      <Switch>
-        <Route path={['/', '/:index']} exact={true} render={() => <Main />} />
-        <Route path="/item/:id" exact={true} render={() => <Comments />} />
-      </Switch>
-  );
+    return (
+        <Switch>
+            <Route path={['/', '/:index']} exact={true} render={() => <Main />} />
+            <Route path="/item/:id" exact={true} render={() => <Comments />} />
+        </Switch>
+    );
 }
 
 export default App;
