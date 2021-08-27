cce440c53a8138054566a572049a2c24719b081c
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-27 15:58:03 +0430

    bug fix link to root

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "styles/assets/logo.gif";
 import "./navbar.css";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <img src={logo} alt="Hacker News logo" className="navbar-logo" />
-            <a href="/" className="navbar-brand">Hacker News</a>
+            <Link to="/" >
+                <span className="navbar-brand">Hacker News</span>
+            </Link>
         </nav>
     );
 };
