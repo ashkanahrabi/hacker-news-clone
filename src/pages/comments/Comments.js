6ba082dfc65be5b718f5cd4321324c52bb094dd2
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-27 16:00:04 +0430

    refactor

--- src/pages/comments/Comments.js
+++ src/pages/comments/Comments.js
@@ -1,20 +1,22 @@
 import React, {useEffect, useState} from 'react';
-import Navbar from "../../components/navbar/Navbar";
-import News from "../../components/news/News";
+import Navbar from "components/navbar/Navbar";
+import News from "components/news/News";
 import {useHistory} from "react-router-dom";
-import {dataFetch} from "../../utils/dataFetch";
-import List from "../../components/List/List";
+import {dataFetch} from "utils/dataFetch";
+import List from "components/list/List";
 
 const Comments = () => {
     const history = useHistory();
     const newsId = history.location.pathname.split('/')[2];
     const [commentList, setCommentList] = useState(null);
+
     useEffect(() => {
         dataFetch(`/item/${newsId}`)
             .then((result) => {
                 setCommentList(result.kids);
             });
     }, []);
+
     return (
         <>
             <Navbar />
