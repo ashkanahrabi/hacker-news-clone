df6940293ac6e0302ce53132852d44c759c327b0
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-27 14:20:04 +0430

    comments page init

---
+++ src/pages/comments/Comments.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import Navbar from "../../components/navbar/Navbar";
+import News from "../../components/news/News";
+import {useHistory} from "react-router-dom";
+import {dataFetch} from "../../utils/dataFetch";
+import List from "../../components/List/List";
+
+const Comments = () => {
+    const history = useHistory();
+    const newsId = history.location.pathname.split('/')[2];
+    const [commentList, setCommentList] = useState(null);
+    useEffect(() => {
+        dataFetch(`/item/${newsId}`)
+            .then((result) => {
+                setCommentList(result.kids);
+            });
+    }, []);
+    return (
+        <>
+            <Navbar />
+            <News item={newsId}/>
+            {
+                commentList?.map(comment => <List item={comment} key={comment} />)
+            }
+        </>
+    );
+};
+
+export default Comments;
