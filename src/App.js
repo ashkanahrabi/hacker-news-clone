f83d8d60d0e1cf6ba452523a1177ea4b06e5e68d
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-27 14:19:22 +0430

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import {Route, Switch} from "react-router-dom";
 import Main from "pages/main/Main";
+import Comments from "pages/comments/Comments";
 
 function App() {
   return (
-      <div className="App">
-        <Main />
-      </div>
+      <Switch>
+        <Route path={['/', '/:index']} exact={true} render={() => <Main />} />
+        <Route path="/item/:id" exact={true} render={() => <Comments />} />
+      </Switch>
   );
 }
 
