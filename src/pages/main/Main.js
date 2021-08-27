2b85dd9055f313d930e28f2ca06f7364659ac65b
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-27 09:54:35 +0430

    render news component

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "components/navbar/Navbar";
+import News from "../../components/news/News";
 
 const Main = () => {
     return (
         <>
             <Navbar />
+            <News />
         </>
     );
 };
