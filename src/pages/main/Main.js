a85e7dca8d60136642c97dc2721e19c978d0a6f6
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-27 16:01:03 +0430

    more button functionality

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -3,24 +3,33 @@ import Navbar from "components/navbar/Navbar";
 import News from "../../components/news/News";
 import {dataFetch} from "../../utils/dataFetch";
 import {useHistory} from "react-router-dom";
+import Button from "../../components/button/Button";
 
 const Main = () => {
     const history = useHistory();
-    const pageNumber = history.location.pathname || 1;
-
+    const pageNumber = Number(history.location.pathname.split('/')[1]) || 1;
     const [newsList, setNewsList] = useState(null);
+
     useEffect(() => {
         dataFetch('/topstories')
             .then((result) => {
-                setNewsList(result.splice((pageNumber - 1) * 30 ,(pageNumber - 1) * 30 + 29));
+                setNewsList(result.splice((pageNumber - 1) * 30 ,30));
             });
-    }, []);
+    }, [history.location.pathname]);
+
+    const incrementPage = () => {
+        history.push(`${pageNumber + 1}`)
+    }
+
     return (
         <>
             <Navbar />
             {
-                newsList?.map(item => <News item={item} key={item}/>)
+                newsList?.map((item, index) => <News item={item} index={(pageNumber - 1) * 30 + index + 1} key={item}/>)
             }
+            <div className="main-button-wrapper">
+                <Button title={'More'} onClick={incrementPage}/>
+            </div>
         </>
     );
 };
