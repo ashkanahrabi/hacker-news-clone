d7712ebfa0bc9024617b0712e48199a94ffaab9c
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-27 12:35:09 +0430

    integration with firebase for news list

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from "components/navbar/Navbar";
 import News from "../../components/news/News";
+import {dataFetch} from "../../utils/dataFetch";
 
 const Main = () => {
+    const [newsList, setNewsList] = useState(null);
+    useEffect(() => {
+        dataFetch('/topstories')
+            .then((result) => {
+                setNewsList(result.splice(0,29));
+            });
+    }, []);
     return (
         <>
             <Navbar />
-            <News />
+            {
+                newsList?.map(item => <News item={item} key={item}/>)
+            }
         </>
     );
 };
