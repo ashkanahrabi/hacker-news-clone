c2a580ef6a0c8525e15264a4d6e26536b3a8b58b
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-27 12:34:07 +0430

    integration with firebase for news details

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './news.css';
+import {dataFetch} from "../../utils/dataFetch";
 
-const News = () => {
+const News = React.memo(({item}) => {
+    const [details, setDetails] = useState(null);
+
+    useEffect(() => {
+        dataFetch(`/item/${item}`)
+            .then((result) => {
+                setDetails(result);
+            });
+    }, []);
+
+    const generateUrl = () => {
+        const url = details?.url?.split('/');
+        return url ? url[2] : '';
+    }
+
     return (
         <>
             <div className="news">
                 <div className="news-title">
                     <div className="font-title-small">1. </div>
-                    <div className="font-title-medium"> Test title </div>
-                    <a className="font-title-small" href="https://test.com">test.com</a>
+                    <div className="font-title-medium">{details?.title}</div>
+                    <a className="font-title-small" href={details?.url}>{`(${generateUrl()})`}</a>
                 </div>
                 <div className="news-subtitle">
-                    <div className="font-subtitle-small">6 points</div>
-                    <a className="font-subtitle-small" href="https://test.com">by tomte</a>
+                    <div className="font-subtitle-small">{details?.score} points</div>
+                    <a className="font-subtitle-small" href="https://test.com">by {details?.by}</a>
                     <a className="font-subtitle-small" href="https://test.com">8 hours ago</a>
-                    <a className="font-subtitle-small" href="https://test.com">0 comments</a>
+                    <a className="font-subtitle-small" href="https://test.com">{details?.descendants} comments</a>
                 </div>
             </div>
         </>
     );
-};
+});
 
 export default News;
