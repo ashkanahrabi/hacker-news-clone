727fba33bc7023fcb36cc874c3972d24ad83608f
Ashkan Ahrabi <ai.ashkan9473@gmail.com>
2021-08-27 14:18:58 +0430

    add useHistory for handling different main pages

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -2,13 +2,17 @@ import React, {useState, useEffect} from 'react';
 import Navbar from "components/navbar/Navbar";
 import News from "../../components/news/News";
 import {dataFetch} from "../../utils/dataFetch";
+import {useHistory} from "react-router-dom";
 
 const Main = () => {
+    const history = useHistory();
+    const pageNumber = history.location.pathname || 1;
+
     const [newsList, setNewsList] = useState(null);
     useEffect(() => {
         dataFetch('/topstories')
             .then((result) => {
-                setNewsList(result.splice(0,29));
+                setNewsList(result.splice((pageNumber - 1) * 30 ,(pageNumber - 1) * 30 + 29));
             });
     }, []);
     return (
